558f85de856521b2d352b9f93b29a07cbb262bfe
ksenia-chernova <ksenia-chernva@yandex.ru>
2026-03-02 12:30:10 +0500

    something

--- src/main.py
+++ src/main.py
@@ -2,9 +2,22 @@ import os
 from dotenv import load_dotenv
 
 import telebot
+from telebot import types
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 if __name__ == "__main__":
-    print(BOT_TOKEN)
+    bot = telebot.TeleBot(BOT_TOKEN)
+
+    @bot.message_handler(commands=['start'])
+    def start_message(message):
+        bot.send_message(message.chat.id,'Привет')
+
+    @bot.message_handler(commands=['button'])
+    def button_message(message):
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Кнопка")
+        markup.add(item1)
+    
+    bot.infinity_polling()
